80499c74336e4938ddbf741abf20c2a3dd28786f
pbhatnagar3 <pbhatnagar3@gatech.edu>
2014-10-26 02:13:58 -0400

    Jacobian and basic coordinate scheme

---
+++ part 3/getJacobian.m
@@ -0,0 +1,14 @@
+function Jacobian = getJacobian(ang,len)
+
+dxq1 = - len(1)*sin(ang(1)) - len(2)*sin(ang(1)+ang(2)) - len(3)*sin(ang(1)+ang(2)+ang(3));
+dxq2 = - len(2)*sin(ang(1)+ang(2)) - len(3)*sin(ang(1)+ang(2)+ang(3));
+dxq3 = - len(3)*sin(ang(1)+ang(2)+ang(3));
+dyq1 = len(1)*cos(ang(1)) + len(2)*cos(ang(1)+ang(2)) + len(3)*cos(ang(1)+ang(2)+ang(3));
+dyq2 = len(2)*cos(ang(1)+ang(2)) + len(3)*cos(ang(1)+ang(2)+ang(3));
+dyq3 = len(3)*cos(ang(1)+ang(2)+ang(3));
+
+
+Jacobian = [dxq1 dxq2 dxq3; dyq1 dyq2 dyq3; 1 1 1];
+
+
+end
---
+++ part 3/getJointPosition.m
@@ -0,0 +1,27 @@
+function jointPos = getJointPosition(l, ang)
+
+jointPos = [0,0;0,0;0,0];
+for i = 1:3
+    
+    jointPos(i, 1) = l(i)*cos(ang(i));
+    jointPos(i, 2) = l(i)*sin(ang(i));      
+    
+end
+ 
+for i = 2:3
+     jointPos(i, 1) = jointPos(i - 1, 1) + jointPos(i, 1);
+     jointPos(i, 2) = jointPos(i - 1, 2) + jointPos(i, 2);    
+end
+
+ 
+% j1_x = l(1)*cos(ang(1));
+% j1_y = l(1)*sin(ang(1));
+% 
+% j2_x = j1_x + l(2)*cos(ang(2));
+% j2_y = j2_y + l(2)*sin(ang(2));
+% 
+% j3_x = l(1)*cos(ang(1));
+% j3_y = l(1)*sin(ang(1));
+
+
+end
