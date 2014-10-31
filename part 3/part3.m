af7b14473aeb9d4dde892117b9d69dbc5dc464e7
pbhatnagar3 <pbhatnagar3@gatech.edu>
2014-10-31 17:39:50 -0400

    part 3b working

--- part 3/part3.m
+++ part 3/part3.m
@@ -12,7 +12,7 @@ function part3()
     
     %************************************************
     %TODO: we still have to tweak the values of threshold and stepValue
-    threshold = 0.5;
+    threshold = 0.1;
     stepValue = 0.1;
     %************************************************
     
@@ -23,6 +23,14 @@ function part3()
     %stop the while loop
     distanceFromGoal = norm(goalPosition - startPosition);
     
+    %parameters for part b
+    circleCenter = [1.3, 2.5];
+    radius = 1;        
+    ang=0:0.01:2*pi; 
+    xp=radius*cos(ang);
+    yp=radius*sin(ang);
+        
+        
     while (distanceFromGoal > threshold) 
         %find the location of the end point
         endPoint = getEndPosition(thetas, armLengths); 
@@ -31,7 +39,11 @@ function part3()
         %we also have to normalize it by dividing it by the distance
         %note: norm gives the equilidean distance
         delta = (goalPosition - endPoint)/norm(goalPosition - endPoint); 
+        if ((norm(circleCenter - endPoint(1:2))) < radius * 1.3)
         
+            delta = delta * -1;
+            
+        end
         %finding the Jacobian
         J = getJacobian(thetas, armLengths);
         
@@ -66,20 +78,17 @@ function part3()
         endPosition_x = joint2_x + armLengths(3) * cos(current_thetas(1) + current_thetas(2) + current_thetas(3));
         endPosition_y = joint2_y + armLengths(3) * sin(current_thetas(1) + current_thetas(2) + current_thetas(3));
    
-        view(3);
          plot([0.0 ; joint1_x], [0.0 ; joint1_y], 'o-'); hold on;
 		 plot([joint1_x ; joint2_x], [joint1_y ; joint2_y], 'ro-'); hold on;
          plot([joint2_x ; endPosition_x], [joint2_y ; endPosition_y], 'bo-'); hold on;         
-        scatter(startPosition(1), startPosition(2), 100, 'filled');
-        scatter(goalPosition(1), goalPosition(2), 100, 'filled');
-        
-%          [xx, yy, zz] = sphere();
-%          xx = xx + 2;
-%          surf(xx, yy, zz);
-         axis([-5, 5, -5, 5]); hold off;
-%          axis equal
+        scatter(startPosition(1), startPosition(2), 50, 'filled'); hold on;
+        scatter(goalPosition(1), goalPosition(2), 100, 'filled'); hold on;      
+        plot(circleCenter(1)+xp, circleCenter(2)+yp); hold on;
+        axis([-5, 5, -5, 5]); hold off;
+        axis equal
     
-         name = sprintf('3a-%04d',secondCounter);
+
+         name = sprintf('3b-%04d',secondCounter);
          print(gcf,name,'-dpng');
          secondCounter = secondCounter + 1;
     end
